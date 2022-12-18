c9ef7ba0b3f42f6339ef63411a08f3aeb5ee3e38
Yoon Gwan <blackgar@naver.com>
2022-12-18 18:25:13 +0900

    feat: API 호출 커스텀 훅 구현
    recoil을 활용해 전역으로 데이터를 관리 및 중복 호출 방지 기능 구현

--- src/hooks/useMutation.tsx
+++ src/hooks/useMutation.tsx
@@ -1,5 +1,41 @@
-function useMutation() {
-  return <div>useMutation</div>;
-}
+import { useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import { useNavigate } from 'react-router-dom';
+import { imageListAtom } from '@/atom';
 
+interface UseMutationState<T> {
+  loading: boolean;
+  data?: T;
+  error?: object;
+  accessToken?: string;
+}
+type UseMutationResult<T> = [(data?: any) => void, UseMutationState<T>];
+
+function useMutation<T = any>(): UseMutationResult<T> {
+  const imageList = useRecoilValue(imageListAtom);
+  const [state, setState] = useState<UseMutationState<T>>({
+    loading: true,
+    data: undefined,
+    error: undefined,
+  });
+
+  function mutation() {
+    if (imageList.length !== 0) return;
+    console.log('Fetch');
+    setState(prev => ({ ...prev, loading: true }));
+    fetch(`${process.env.REACT_APP_API_URL}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        setState(prev => ({ ...prev, data }));
+      })
+      .catch(error => setState(prev => ({ ...prev, error })))
+      .finally(() => setState(prev => ({ ...prev, loading: false })));
+  }
+  return [mutation, { ...state }];
+}
 export default useMutation;
